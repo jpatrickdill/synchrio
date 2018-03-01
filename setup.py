bd1803f566fa9e9b47718ae679dc67e4e6dd0804
Patrick Dill <jamespatrickdill@gmail.com>
2018-02-28 22:00:19 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+__author__ = "J Patrick Dill"
+__version__ = "0.7"
+
+with open("readme.rst", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="synchrio",
+    description="Wrapper class to run async code synchronously",
+    long_description=long_description,
+
+    licence="GPLv3",
+
+    version=__version__,
+    packages=["synchrio"],
+
+    author=__author__,
+    author_email="jamespatrickdill@gmail.com",
+
+
+)
